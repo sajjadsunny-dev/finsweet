c79d6130a65adca22cf0e4050e6643f6ca0c0a20
sajjadsunnyofficial <sajjadhossainsunny.official@gmail.com>
2023-07-17 13:07:10 +0600

    some modification for responsive

--- js/script.js
+++ js/script.js
@@ -9,33 +9,3 @@ btnsArr.map((items) => {
         this.className += " active";
     });
 });
-
-
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// function showSlides(n) {
-//     let clientReviewId = document.querySelector("#client_review");
-//     let slides = clientReviewId.querySelectorAll(".review");
-//     let slidesArr = Array.from(slides);
-
-//     if (n > slidesArr.length) {
-//         slideIndex = 1;
-//     }
-
-//     if (n < 1) {
-//         slideIndex = slidesArr.length;
-//     }
-
-//     slidesArr.map((items) => {
-//         items.style.display = "none";
-
-//     });
-
-//     slidesArr[slideIndex - 1].style.display = "block";
-// }
